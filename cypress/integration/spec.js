4e73637617b6502110f2e5b825bb3fd345a726f8
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2018-08-04 06:30:40 -0400

    add second tiny test

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -4,4 +4,9 @@ describe('tiny blog', () => {
     cy.visit('localhost:5000')
     cy.contains('h1', 'Tiny Blog')
   })
+
+  it('loads 2nd time', () => {
+    cy.visit('localhost:5000')
+    cy.contains('h1', 'Tiny Blog')
+  })
 })
